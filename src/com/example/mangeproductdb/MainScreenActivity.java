4209a53d1b954ae360015f94da843659770e133a
Kevin Shi <kevin@dare2tee.com>
2013-12-28 23:53:21 -0500

    populated mainscreenactivity by writing what the view products and the new products button will do

--- src/com/example/mangeproductdb/MainScreenActivity.java
+++ src/com/example/mangeproductdb/MainScreenActivity.java
@@ -1,22 +1,30 @@
 package com.example.mangeproductdb;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainScreenActivity extends Activity {
 
+	Button btnViewProducts;
+	Button btnNewProduct;
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main_screen);
-	}
+		setContentView(R.layout.main_screen);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main_screen, menu);
-		return true;
-	}
+		btnViewProducts=(Button) findViewById(R.id.btnViewProducts);
+		btnNewProduct=(Button) findViewById(R.id.btnCreateProduct);
 
+		btnViewProducts.setOnClickListener(new View.OnClickListener() {
+			public void onClick (View view) {
+				Intent i = new Intent(getApplicationContext(), AllProductsActivity.class);
+				startActivity(i);
+			}
+		});
+	}
 }

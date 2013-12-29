125bb9bae6f8996ce2d9910aff839ce47ed63407
Kevin Shi <kevin@dare2tee.com>
2013-12-28 23:56:11 -0500

    Created layout for list of all products as all_products.xml

--- gen/com/example/mangeproductdb/R.java
+++ gen/com/example/mangeproductdb/R.java
@@ -30,7 +30,8 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main_screen=0x7f030000;
-        public static final int main_screen=0x7f030001;
+        public static final int all_products=0x7f030001;
+        public static final int main_screen=0x7f030002;
     }
     public static final class menu {
         public static final int main_screen=0x7f070000;

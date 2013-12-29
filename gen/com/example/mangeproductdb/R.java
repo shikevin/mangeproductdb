d6232fe4f76d652a7c63aef30cc4dd099c074d56
Kevin Shi <kevin@dare2tee.com>
2013-12-28 23:42:34 -0500

    created and populated the mainscreen xml with two buttons (res/layout)

--- gen/com/example/mangeproductdb/R.java
+++ gen/com/example/mangeproductdb/R.java
@@ -24,10 +24,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080002;
+        public static final int btnCreateProduct=0x7f080001;
+        public static final int btnViewProducts=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main_screen=0x7f030000;
+        public static final int main_screen=0x7f030001;
     }
     public static final class menu {
         public static final int main_screen=0x7f070000;

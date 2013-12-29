cf3f4a0a586f49c79ef02d19737c1765186a7446
Kevin Shi <kevin@dare2tee.com>
2013-12-29 01:10:51 -0500

    edit product activity layout, edit_product.xml

--- gen/com/example/mangeproductdb/R.java
+++ gen/com/example/mangeproductdb/R.java
@@ -24,18 +24,24 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
+        public static final int action_settings=0x7f080009;
         public static final int btnCreateProduct=0x7f080003;
-        public static final int btnViewProducts=0x7f080002;
-        public static final int name=0x7f080001;
-        public static final int pid=0x7f080000;
+        public static final int btnDelete=0x7f080005;
+        public static final int btnSave=0x7f080004;
+        public static final int btnViewProducts=0x7f080008;
+        public static final int inputDesc=0x7f080002;
+        public static final int inputName=0x7f080000;
+        public static final int inputPrice=0x7f080001;
+        public static final int name=0x7f080007;
+        public static final int pid=0x7f080006;
     }
     public static final class layout {
         public static final int activity_main_screen=0x7f030000;
         public static final int add_product=0x7f030001;
         public static final int all_products=0x7f030002;
-        public static final int list_item=0x7f030003;
-        public static final int main_screen=0x7f030004;
+        public static final int edit_product=0x7f030003;
+        public static final int list_item=0x7f030004;
+        public static final int main_screen=0x7f030005;
     }
     public static final class menu {
         public static final int main_screen=0x7f070000;

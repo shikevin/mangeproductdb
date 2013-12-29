fa3c26730a46421aa9aeb7460d3572822b2ef991
Kevin Shi <kevin@dare2tee.com>
2013-12-28 23:59:57 -0500

    Created layout for displaying an item list as list_item.xml

--- gen/com/example/mangeproductdb/R.java
+++ gen/com/example/mangeproductdb/R.java
@@ -24,14 +24,17 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int btnCreateProduct=0x7f080001;
-        public static final int btnViewProducts=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int btnCreateProduct=0x7f080003;
+        public static final int btnViewProducts=0x7f080002;
+        public static final int name=0x7f080001;
+        public static final int pid=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main_screen=0x7f030000;
         public static final int all_products=0x7f030001;
-        public static final int main_screen=0x7f030002;
+        public static final int list_item=0x7f030002;
+        public static final int main_screen=0x7f030003;
     }
     public static final class menu {
         public static final int main_screen=0x7f070000;
